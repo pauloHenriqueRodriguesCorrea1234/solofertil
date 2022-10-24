9d84a44d722610b2a7b12e050047c55bd03be634
cecilya <cecilya.ribeiro@ifms.edu.br>
2022-10-24 19:28:17 -0400

    atualização forgot1 feita

--- src/pages/FotgotPassword/index.jsx
+++ src/pages/FotgotPassword/index.jsx
@@ -1,9 +1,22 @@
-import { View, Text } from "./style"
+import { View, TextInput,TouchableOpacityLogin, TextLogin } from "./style";
+import Header from "../../components/Header";
+export default function ForgotPassword() {
+  return (
+    <View>
+      <Header />
 
-export default function ForgotPassword () {
-    return(
-        <View>
-           <Text>Tela ainda não foi feita</Text>
-        </View>
-    )
+      <TextInput placeholderTextColor="#FFF"
+        placeholder="Rsest password"/>
+
+      <Text>Enter the e-mail associed with your account and we’ll send an e-mail with  instructions to reset your password.</Text>
+
+      <TextInput placeholderTextColor="#FFF"
+        placeholder="E-Mail Addres"/>
+
+<TouchableOpacityLogin onPress={() => navigation.push("Login")}>
+        <TextLogin>LOGIN</TextLogin>
+      </TouchableOpacityLogin>
+    </View>
+
+  );
 }
--- src/pages/FotgotPassword/style.js
+++ src/pages/FotgotPassword/style.js
@@ -6,3 +6,22 @@ flex: 1;
 export const Text = styled.Text`
 
 `
+export const TextInput = styled.TextInput`
+  height: 40px;
+  margin: 20px;
+  padding: 10px;
+  font-weight: 2;
+  color: #fff;
+  background-color: #022508;
+  border-bottom-width: 1px;
+  border-bottom-color: #fff;
+
+`;
+
+export const TouchableOpacityLogin = styled.TouchableOpacity`
+  align-items: center;
+  margin: 10% 23%;
+`;
+export const TextLogin = styled.Text`
+  color: #fff;
+`;
